a4464e63d5d461e4ca7b02bc4be828d53f4a008c
nklharish1 <nklharish1@gmail.com>
2025-11-12 11:50:56 +0530

    exp

--- php/update_expenses.php
+++ php/update_expenses.php
@@ -16,24 +16,24 @@ echo $exp_date;
 echo $exp_emp_id;
 echo $exp_id;
 
-function test_input($data)
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  $data = "'" . $data . "'";
-}
+// function test_input($data)
+// {
+//   $data = trim($data);
+//   $data = stripslashes($data);
+//   $data = htmlspecialchars($data);
+//   $data = "'" . $data . "'";
+// }
 
 
 
-$sql = "UPDATE expense SET exp_des = $exp_des,exp_cat = $exp_cat,exp_amount = $exp_amount,exp_date = $exp_date,exp_emp_id  = $exp_emp_id where exp_id = $exp_id";
+// $sql = "UPDATE expense SET exp_des = $exp_des,exp_cat = $exp_cat,exp_amount = $exp_amount,exp_date = $exp_date,exp_emp_id  = $exp_emp_id where exp_id = $exp_id";
   
-  if ($conn->query($sql) === TRUE) {
+//   if ($conn->query($sql) === TRUE) {
    
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  echo "ok";
+//   } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+//   }
+//   echo "ok";
   
 
 
