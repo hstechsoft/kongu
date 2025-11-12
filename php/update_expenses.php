898850eac5b20b9df308a9ea4b6ef15f3bb05b20
nklharish1 <nklharish1@gmail.com>
2025-11-12 12:00:19 +0530

    exp corrected

--- php/update_expenses.php
+++ php/update_expenses.php
@@ -2,40 +2,33 @@
 <?php
  include 'db_head.php';
 
-$exp_des =test_input($_GET['exp_des']);
-    $exp_cat =test_input($_GET['exp_cat']);
-    $exp_amount =test_input($_GET['exp_amount']);
-    $exp_date =test_input($_GET['exp_date']);
-     $exp_emp_id =test_input($_GET['exp_emp_id']);
-$exp_id =test_input($_GET['exp_id']);
+$exp_des = test_input($_GET['exp_des']);
+    $exp_cat = test_input($_GET['exp_cat']);
+    $exp_amount = test_input($_GET['exp_amount']);
+    $exp_date = test_input($_GET['exp_date']);
+     $exp_emp_id = test_input($_GET['exp_emp_id']);
+$exp_id = test_input($_GET['exp_id']);
 
 
-echo "hi";
-echo $exp_des;
-echo $exp_cat;    
-echo $exp_amount;
-echo $exp_date;
-echo $exp_emp_id;
-echo $exp_id;
 
-function test_input($data)
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  $data = "'" . $data . "'";
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+$data = "'".$data."'";
+return $data;
 }
 
 
 
-// $sql = "UPDATE expense SET exp_des = $exp_des,exp_cat = $exp_cat,exp_amount = $exp_amount,exp_date = $exp_date,exp_emp_id  = $exp_emp_id where exp_id = $exp_id";
+$sql = "UPDATE expense SET exp_des = $exp_des,exp_cat = $exp_cat,exp_amount = $exp_amount,exp_date = $exp_date,exp_emp_id  = $exp_emp_id where exp_id = $exp_id";
   
-//   if ($conn->query($sql) === TRUE) {
+  if ($conn->query($sql) === TRUE) {
    
-//   } else {
-//     echo "Error: " . $sql . "<br>" . $conn->error;
-//   }
-//   echo "ok";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  echo "ok";
   
 
 
